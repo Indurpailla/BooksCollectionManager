5ef22fd4f8a05f72742e00fe00aa274ad5b04fae
indureddypailla <indupailla@gmail.com>
2023-05-30 10:08:48 -0400

    Fixed Warnings

--- BooksCollectionManager/Models/Book.cs
+++ BooksCollectionManager/Models/Book.cs
@@ -4,11 +4,11 @@ namespace BooksCollectionManager.Models
 	public class Book
 	{
         public int BookId { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Genre { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Genre { get; set; }
         public int PublicationYear { get; set; }
-        public BookStatus Status { get; set; }
+        public BookStatus? Status { get; set; }
 
     }
 }
